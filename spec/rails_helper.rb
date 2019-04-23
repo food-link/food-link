33638ea4c204ad2df9d5d0022aae24ac417c5d2c
Kainan Zhang <kzhang20@hawk.iit.edu>
2019-04-23 18:48:46 -0400

    user guide

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,10 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  SimpleCov.coverage_dir('public/coverage')
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
